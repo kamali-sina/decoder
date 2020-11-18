c04fb00042d90c29a2c9913e4b255a81b1ea6bb1
Sina Kamali <sininoir@gmail.com>
2020-11-18 22:49:07 +0330

    added helper string

--- meaning_check.py
+++ meaning_check.py
@@ -1,11 +1,13 @@
 from PyDictionary import PyDictionary
 
+helper = ['i', 'a']
+
 def meaning_check(dupped_input_string):
-    threshold = 0.8
+    threshold = 0.6
     dictionary = PyDictionary()
     i = 0
     for x in dupped_input_string:
-        if (dictionary.meaning(x, disable_errors=True) != None):
+        if (dictionary.meaning(x, disable_errors=True) != None or x in helper):
             i += 1
     if ((i / len(dupped_input_string)) > threshold):
         return True
