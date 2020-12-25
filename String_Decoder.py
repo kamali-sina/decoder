996e63c0b7b1bb340a02ab49be7162c242bdb0df
Sina Kamali <sininoir@gmail.com>
2020-12-25 23:49:14 +0330

    updated code to support morse code decoding

--- String_Decoder.py
+++ String_Decoder.py
@@ -1,6 +1,7 @@
 from _helpers import kb_map
 from meaning_check import meaning_check
 from tqdm import tqdm
+from morse_code import decode_with_space_inject, decode_morse_code
 
 class String_Decoder:
     def __init__(self, split_by=' ', words=None):
@@ -58,7 +59,28 @@ class String_Decoder:
             print('printing all: \n')
             self._print_list_of_duppeds(result)
 
+    def _morse_code(self, input_string):
+        splitted_string = input_string.split()
+        message = decode_morse_code(splitted_string)
+        if (len(message) > 0):
+            print(f'decoded morse code: {message}')
+            return
+        ans = input('\nfailed to decode morse code, do you want to brute force attack(y/n)? ')
+        if (ans.strip().lower() == 'n' or ans.strip().lower() == 'no'):
+            print('continuing...')
+            return
+        print('\nstarting brute force attack...')
+        ofile = open('./morse_code_result.txt', 'w')
+        # '\n'.join(lines) + '\n'
+        for num_of_spaces in tqdm(range(len(input_string))):
+            decodes = decode_with_space_inject(input_string, num_of_spaces)
+            ofile.write(f'decoded with {num_of_spaces} spaces:\n')
+            ofile.write('\n'.join(decodes) + '\n')
+        ofile.close()
+        print('\nall decoded codes can be found in "./morse_code_result.txt"')
+
     def decode(self, input_string):
         dupped_input = input_string.split(self.split_by)
         self._english_kb_to_persian(dupped_input)
         self._caesar_cipher(dupped_input)
+        self._morse_code(input_string)
