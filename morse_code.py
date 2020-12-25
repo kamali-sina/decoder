97b43b9e601fdfb14388afb472c29b36201b1548
Sina Kamali <sininoir@gmail.com>
2020-12-25 23:46:28 +0330

    code to handle morse code logic

---
+++ morse_code.py
@@ -0,0 +1,31 @@
+from _helpers import MORSE_CODE_DICT
+from itertools import combinations
+key_list = list(MORSE_CODE_DICT.keys())
+value_list = list(MORSE_CODE_DICT.values())
+
+def decode_morse_code(splitted_string):
+    message = ''
+    for part in splitted_string:
+        try:
+            message += key_list[value_list.index(part)]
+        except:
+            return ''
+    return message
+
+def decode_with_space_inject(string, num_of_spaces):
+    all_messages = []
+    locations = list(range(1,len(string)))
+    all_combinations = combinations(locations, num_of_spaces)
+    for state in all_combinations:
+        splitted_string = []
+        last_location = 0
+        for i in range(len(state)):
+            splitted_string.append(string[last_location:state[i]])
+            last_location = state[i]
+        splitted_string.append(string[last_location:])
+        message = decode_morse_code(splitted_string)
+        if (len(message) > 0):
+            all_messages.append(message)
+    return all_messages
+
+# ..-.-...--
